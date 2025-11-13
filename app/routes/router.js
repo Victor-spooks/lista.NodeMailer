d857cc0b95623707c913cc0b86c98aad9945bac9
Victor Carneiro do Nascimento <rm94717@estudante.fieb.edu.br>
2025-11-13 10:00:15 -0300

    memziNHaão

--- app/routes/router.js
+++ app/routes/router.js
@@ -2,6 +2,19 @@ var express = require("express");
 var router = express.Router();
 const { body, validationResult } = require("express-validator");
 
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "cvictor7n@gmail.com",
+    pass: "crcy ugoy qxdk toxe"
+  }
+
+})
+
 router.get("/", function (req, res) {
   res.render("pages/index", {
     resultado: null,
@@ -10,8 +23,8 @@ router.get("/", function (req, res) {
       nome: "",
       email: "",
       telefone: "",
-      AssuntoDamenssagem:"",
-      Menssagem:""
+      AssuntoDamenssagem: "",
+      Menssagem: ""
     }
   });
 });
@@ -27,9 +40,9 @@ router.post(
       .isEmail().withMessage("O e-mail deve ser válido!"),
     body("telefone")
       .isLength({ min: 11, max: 11 }).withMessage("O telefone deve ter 11 digitos !!"),
-      body("assunto")
+    body("assunto")
       .isLength({ min: 5, max: 30 }).withMessage("O assunto deve conter no minimo 5 digitos e no maximo 30!"),
-      body("menssagem")
+    body("menssagem")
       .isLength({ min: 5, max: 500 }).withMessage("A messagem deve conter no minimo 5 digitos e no maximo 500!"),
   ],
   function (req, res) {
@@ -42,6 +55,21 @@ router.post(
       });
     }
 
+    const mailOptions = {
+      from: "rm94717@estudante.fieb.edu.br",
+      to: "cvictor7n@gmail.com",
+      subject: "assunto",
+      text: "menssagem em formatos texto",
+      html: "<h1>menssagem em formato HTML</h1>"
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        comsole.log(error)
+      } else {
+        console.log(info)
+        console.log("email enviado")
+      }
+    });
     return res.render("pages/index", {
       resultado: req.body,
       listaErros: null,
